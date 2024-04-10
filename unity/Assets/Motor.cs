9ac201dd1ae07006ff65293f782383a5033e7d0a
Lasse Schier Christiansen <lasse.10@hotmail.dk>
2024-04-10 11:48:52 +0200

    fix: remade unity project with correct gitignores

---
+++ unity/Assets/Motor.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour
+{
+    [SerializeField] private char motorAxis = 'X';
+    [SerializeField] private float minInputValue = 0f;
+    [SerializeField] private float maxInputValue = 2048f;
+    [SerializeField] private float minRotationDegree = 0f;
+    [SerializeField] private float maxRotationDegree = 360f;
+
+    private float initialRotationX;
+    private float initialRotationY;
+    private float initialRotationZ;
+
+    [SerializeField] private float testValue = 0f;
+    [SerializeField] private bool useTestValue = false;
+
+    private void Awake()
+    {
+        initialRotationX = transform.localEulerAngles.x;
+        initialRotationY = transform.localEulerAngles.y;
+        initialRotationZ = transform.localEulerAngles.z;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateObject(UDPManager.Instance.motorXValue);
+    }
+
+    private void RotateObject(float value)
+    {
+        float mappedRotation = MapValue(value, minInputValue, maxInputValue, minRotationDegree, maxRotationDegree);
+
+        Vector3 rotationVector = new Vector3(initialRotationX, mappedRotation, initialRotationZ);
+
+        transform.localRotation = Quaternion.Euler(rotationVector);
+    }
+
+    private float MapValue(float value, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        return Mathf.Lerp(toMin, toMax, Mathf.InverseLerp(fromMin, fromMax, value));
+    }
+}
